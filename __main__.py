9706b7bfea635de00fc37e9610d442c473e58089
Vojtech Kotek <kotek.vojtech@gmail.com>
2016-09-14 02:09:38 +0200

    renaming

--- __main__.py
+++ __main__.py
@@ -3,6 +3,8 @@
 # FOR EACH USER GET WORDS, GET DEFS, EMAIL IT
 
 import configparser
+import mailer
+
 
 users_file = 'users.ini'
 users = configparser.RawConfigParser()
@@ -10,13 +12,17 @@ users.read(users_file)
 
 # FOR EACH USER
 for user in users.sections():
-    
 
     # GET USER CONFIG
-    
+    username = users.get(user, 'username')
+    email = users.get(user, 'email')
+    lang = users.get(user, 'lang')
+    step = users.get(user, 'step')
+    cursor = users.get(user, 'cursor')
     
 
     # GET WORDS
+    
 
     # GET DEFINITIONS
 
